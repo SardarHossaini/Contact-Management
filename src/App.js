aefe4fdf332386f98f18bbc51353c8794e75062f
Sardar219 <sardarhossaini2022@gmail.com>
2025-03-09 22:22:28 +0430

    added contacts & contact components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Contacts from "./components/contact/Contacts.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Contacts />
     </div>
   );
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,49 @@
+import { CURRENTLINE, CYAN, ORANGE, PURPLE, RED } from "../../helpers/colors";
+
+const Contact = () => {
+  return (
+    <div className="col-md-6 col-6">
+      <div className="card my-2" style={{ backgroundColor: CURRENTLINE }}>
+        <div className="card-body">
+          <div className="row align-items-center d-flex justify-content-around">
+            <div className="col-md-4 col-sm-4">
+              <img
+                src="assets/sardar.jpg"
+                alt=""
+                style={{ border: `1px solid ${PURPLE}` }}
+                className="img-fluid rounded"
+              />
+            </div>
+            <div className="col-md-7 col-sm-7">
+              <ul className="list-group">
+                <li className="list-group-item list-group-item-dark">
+                  Full Name: <span className="fw-bold">Sardar Hossaini</span>
+                </li>
+                <li className="list-group-item list-group-item-dark">
+                  Phone No: <span className="fw-bold">0782148885</span>
+                </li>
+                <li className="list-group-item list-group-item-dark">
+                  Email:{" "}
+                  <span className="fw-bold">sardarhossaini2022@gmail.com</span>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-1 col-sm-1 d-flex flex-md-column align-items-center flex-row">
+              <button className="btn my-1" style={{ backgroundColor: ORANGE }}>
+                <i className="fa fa-eye"></i>
+              </button>
+              <button className="btn my-1" style={{ backgroundColor: CYAN }}>
+                <i className="fa fa-pen"></i>
+              </button>
+              <button className="btn my-1" style={{ backgroundColor: RED }}>
+                <i className="fa fa-trash"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
